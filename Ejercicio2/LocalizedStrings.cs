e5d05ebde550387cd815d002fbd09c2f673f27c2
Edgar Alonso Vidal Sarria <eavidal@unicauca.edu.co>
2015-03-16 12:07:59 -0500

    botones

---
+++ Ejercicio2/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Ejercicio2.Resources;
+
+namespace Ejercicio2
+{
+    /// <summary>
+    /// Proporciona acceso a los recursos de cadena.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Ejercicio2.Resources;
+using System.Windows.Media;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void uno(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush nuevo=new SolidColorBrush();
+           
+            if((Amarillo.IsChecked == true) && (Azul.IsChecked == true) && (Rojo.IsChecked == true))
+            {
+                nuevo.Color = Colors.White;
+                r1.Fill = nuevo;
+            }
+            
+            if((Amarillo.IsChecked == false) && (Azul.IsChecked == false) && (Rojo.IsChecked == false))
+            {
+                nuevo.Color = Colors.Transparent;
+                r1.Fill = nuevo;
+            }
+           
+            if ((Amarillo.IsChecked == true) && (Azul.IsChecked == false) && (Rojo.IsChecked == false))
+            {
+                nuevo.Color = Colors.Yellow;
+                r1.Fill = nuevo;
+            }
+            
+            if ((Amarillo.IsChecked == false) && (Azul.IsChecked == true) && (Rojo.IsChecked == false))
+            {
+                nuevo.Color = Colors.Blue;
+                r1.Fill = nuevo;
+            }
+            
+            if ((Amarillo.IsChecked == false) && (Azul.IsChecked == false) && (Rojo.IsChecked == true))
+            {
+                nuevo.Color = Colors.Red;
+                r1.Fill = nuevo;
+            }
+                       
+            if ((Amarillo.IsChecked == false) && (Azul.IsChecked == true) && (Rojo.IsChecked == true))
+            {
+                nuevo.Color = Colors.Purple;
+                r1.Fill = nuevo;
+            }
+            if ((Amarillo.IsChecked == true) && (Azul.IsChecked == false) && (Rojo.IsChecked == true))
+            {
+                nuevo.Color = Colors.Orange;
+                r1.Fill = nuevo;
+            }
+            if ((Amarillo.IsChecked == true) && (Azul.IsChecked == true) && (Rojo.IsChecked == false))
+            {
+                nuevo.Color = Colors.Green;
+                r1.Fill = nuevo;
+            }     
+        }
+
+         
+
+       
+    }
+}
